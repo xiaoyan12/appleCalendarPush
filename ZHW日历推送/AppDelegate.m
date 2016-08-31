9a3f292104414df8c030a2bc89612f6209835e9f
dorayo <yomingyo@gmail.com>
2016-08-31 15:08:44 +0800

    Initial Commit

---
+++ ZHW日历推送/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ZHW日历推送
+//
+//  Created by 闫世超 on 16/8/31.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ ZHW日历推送Tests/ZHW____Tests.m
@@ -0,0 +1,39 @@
+//
+//  ZHW____Tests.m
+//  ZHW日历推送Tests
+//
+//  Created by 闫世超 on 16/8/31.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZHW____Tests : XCTestCase
+
+@end
+
+@implementation ZHW____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
